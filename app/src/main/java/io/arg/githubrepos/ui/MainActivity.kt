d021ede145720061fd44920a253fc9cd10a95eda
Athanasios <athanasiosgerostamoulos@gmail.com>
2021-04-22 17:49:26 +0300

    First commit

---
+++ app/src/main/java/io/arg/githubrepos/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package io.arg.githubrepos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.arg.githubrepos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
